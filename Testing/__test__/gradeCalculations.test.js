3ba64930facc2707ae5d0b674590d8b1e82e512b
Naqi <sunanhaqi.sh@gmail.com>
2023-04-01 12:45:15 +0700

    fourth commit

--- Testing/__test__/gradeCalculations.test.js
+++ Testing/__test__/gradeCalculations.test.js
@@ -1,11 +1,25 @@
-const { averageExams } = require('../gradeCalculations');
+const { averageExams, isStudentPassExam } = require('../gradeCalculations');
  
-test('it should return exact average', () => {
-    const listValueOfExams = [80, 100, 100, 80];
-    expect(averageExams(listValueOfExams)).toEqual(90);
-})
+describe('grade calculations', () => {
 
-test('it should handle non-number ', () => {
-    const listValueOfExams = [80, 'a', 100, 80];
-    expect(() => averageExams(listValueOfExams)).toThrow();
+    test('it should return exact average', () => {
+        const listValueOfExams = [80, 100, 100, 80];
+        expect(averageExams(listValueOfExams)).toEqual(90);
+    })
+    
+    test('it should handle non-number ', () => {
+        const listValueOfExams = [80, 'a', 100, 80];
+        expect(() => averageExams(listValueOfExams)).toThrow();
+    })
+
+    //Integration testing
+    test('it should return exam passed status', () => {
+        const listValueOfExams = [80, 100, 100, 80];
+        expect(isStudentPassExam(listValueOfExams, 'Budi')).toEqual(true);
+    })
+
+    test('it should return exam failed status', () => {
+        const listValueOfExams = [50, 40, 70, 80];
+        expect(isStudentPassExam(listValueOfExams, 'Budi')).toEqual(false);
+    })
 })
