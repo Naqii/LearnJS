0ac68d3021a0354cbd8f8b19cf74b9b3158906c4
Naqi <sunanhaqi.sh@gmail.com>
2023-04-01 12:38:50 +0700

    fourth commit

--- Testing/__test__/gradeCalculations.test.js
+++ Testing/__test__/gradeCalculations.test.js
@@ -4,3 +4,8 @@ test('it should return exact average', () => {
     const listValueOfExams = [80, 100, 100, 80];
     expect(averageExams(listValueOfExams)).toEqual(90);
 })
+
+test('it should handle non-number ', () => {
+    const listValueOfExams = [80, 'a', 100, 80];
+    expect(() => averageExams(listValueOfExams)).toThrow();
+})
